8220343f1e63a89d4ba92ca791303c60df67c917
Antonio <antondm11@alumnos.ilerna.com>
2026-01-26 23:58:11 +0100

    Creación de MavenProject. Incorporación de códigos de clase Main y Test

---
+++ ActividadOptimizacionMaven/src/main/java/ilerna/ActividadOptimizacionMaven/ProcesadorPedidos.java
@@ -0,0 +1,13 @@
+package ilerna.ActividadOptimizacionMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class ProcesadorPedidos 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
